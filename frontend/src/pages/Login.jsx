268421cbe5b865be3cb4567ee2257c7c5cd3aef3
syeef <syeefislam@yahoo.com>
2024-04-24 10:53:29 +0530

    login and register done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Form from "../components/Form";
 
 function Login() {
-  return <div>Login</div>;
+  return <Form route="/api/token/" method="login" />;
 }
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Form from "../components/Form";
 
 function Register() {
-  return <div>Register</div>;
+  return <Form route="/api/user/register/" method="register" />;
 }
 
 export default Register;
